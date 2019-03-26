fa67bdfd372f7ea6bb2840ed07ca08247fd10678
Heijns <>
2019-03-25 23:25:19 -0400

    Add naics columns

--- prep/all/run.sh
+++ prep/all/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # To run this bash script, enter command: ./run.sh
-echo 'Generating csv output. This may take a minute.'
+echo 'Generating output for docs/data/2017/zcta_out.csv. This may take a minute.'
 sqlite3 zcta.db < zcta.SQL.txt > zcta.OUT.txt
 rm zcta.db
-echo 'Results sent to docs/data/2017/zcta_out.csv'
+#echo 'Results sent to docs/data/2017/zcta_out.csv'
